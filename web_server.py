79426bf4c6ac51b57aa0a6328c0bd36a044c3e92
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2025-01-27 16:26:18 -0800

    add comments

--- web_server.py
+++ web_server.py
@@ -4,24 +4,27 @@ import sys  # In order to terminate the program
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 # Prepare a sever socket
-HOST = ''
+HOST = 'localhost'
 PORT = 6789
 serverSocket.bind((HOST, PORT))
 serverSocket.listen(1)
 print(f"Server is listening on port {PORT}... waiting for one client")
 
 while True:
-    # Establish the connection
+    # Establish the connection when contacted by a client (browser)
     print('Ready to serve...')
     connectionSocket, addr = serverSocket.accept()
     print(f"Connection established with client {addr}")
     try:
+        # Receive the HTTP request
         message = connectionSocket.recv(2048).decode()
+        # Parse the request to determine file name to be processed
         filename = message.split()[1]
+        # Get the requested file from server's file system
         f = open(filename[1:])
         outputdata = f.read()
         # Send one HTTP header line into socket
-        header = "HTTP/1.1 200 OK\nContent-Type: text/html;\r\n\n"
+        header = "HTTP/1.1 200 OK\r\n\n"
         connectionSocket.send(header.encode())
         # Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
@@ -31,7 +34,7 @@ while True:
         connectionSocket.close()
     except IOError:
         # Send response message for file not found
-        header = ("HTTP/1.1 404 Not Found\n")
+        header = "HTTP/1.1 404 Not Found\n"
         connectionSocket.send(header.encode())
         # Close client socket
         connectionSocket.close()
