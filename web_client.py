042d7440411577a1b9ee60a9470dbf951f259879
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2025-01-27 16:26:54 -0800

    get filename from cmd line

--- web_client.py
+++ web_client.py
@@ -1,27 +1,22 @@
-import socket
+import socket, sys
 
 # Create a socket object
 clientSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 # Connect to the server
-HOST = ''
+HOST = 'localhost'
 PORT = 6789
 clientSocket.connect((HOST, PORT))
 print("Connected to the server")
 
-message = ("GET /HelloWorld.html HTTP/1.1\n"
-"Host: 10.152.196.97:6789\n"
-"Connection: keep-alive\n"
-"DNT: 1\n"
-"Upgrade-Insecure-Requests: 1\n"
-"User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36\n"
-"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\n"
-"Accept-Encoding: gzip, deflate\n"
-"Accept-Language: en-US,en;q=0.9,zh;q=0.8,zh-TW;q=0.7,zh-CN;q=0.6,ko;q=0.5\n")
-clientSocket.send(message.encode()) # Send the message to the server
+# Test a page
+filename = sys.argv[1]
+print(f'File path: {filename}')
+http_request = (f"GET /{filename} HTTP/1.1\n")
+clientSocket.send(http_request.encode())
+http_response = clientSocket.recv(2048).decode().split('\n')[0]
+print(http_response)
 
-response = clientSocket.recv(2048).decode()
-print(response)
     
 # Close the connection
 clientSocket.close()
